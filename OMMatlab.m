2a9b8d44e853e2b1e129d40119d7acd31662799e
arun3688 <rain100falls@gmail.com>
2018-04-09 12:36:20 +0200

    fix system cmd with char representation

--- OMMatlab.m
+++ OMMatlab.m
@@ -11,13 +11,14 @@ classdef OMMatlab < handle
             if ispc
                 omhome = getenv('OPENMODELICAHOME');
                 omhomepath = replace(fullfile(omhome,'bin','omc.exe'),'\','/');
-                cmd ="START /b "+omhomepath +" --interactive=zmq +z=matlab."+randomstring;
+                %cmd ="START /b "+omhomepath +" --interactive=zmq +z=matlab."+randomstring;
+                cmd = ['START /b',' ',omhomepath,' --interactive=zmq +z=matlab.',randomstring];
                 portfile = strcat('openmodelica.port.matlab.',randomstring);
             else
                 if ismac && system("which omc") ~= 0
-                  cmd ="/opt/openmodelica/bin/omc --interactive=zmq -z=matlab."+randomstring+" &";
+                  cmd =['/opt/openmodelica/bin/omc --interactive=zmq -z=matlab.',randomstring,' &'];
                 else
-                  cmd ="omc --interactive=zmq -z=matlab."+randomstring+" &";
+                  cmd =['omc --interactive=zmq -z=matlab.',randomstring,' &'];
                 end
                 portfile = strcat('openmodelica.',getenv('USER'),'.port.matlab.',randomstring);
             end
